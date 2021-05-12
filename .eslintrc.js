9a4e5a33442bf6bb7ad12cad015f9e1f48df0e43
RubesWorld <hi@rubesworld.live>
2021-05-11 21:14:41 -0700

    fixed linting and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,31 @@
 module.exports = {
-    env: {
-      commonjs: true,
-      browser: true,
-      es6: true,
-      node: true,
+  root: true,
+  env: {
+    commonjs: true,
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-      "eslint:recommended",
-      "plugin:react/recommended",
-      "plugin:prettier/recommended",
-    ],
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {},
+  overrides: [
+    {
+      files: ['**/*.test.js'],
+      env: {
+        jest: true,
       },
-      ecmaVersion: 2018,
-      sourceType: "module",
     },
-    plugins: ["react", "prettier"],
-    rules: {},
-    overrides: [
-      {
-        files: ["**/*.test.js"],
-        env: {
-          jest: true,
-        },
-      },
-    ],
-  };
+  ],
+};
